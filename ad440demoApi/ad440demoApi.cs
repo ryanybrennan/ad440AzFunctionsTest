92d8464780dacb5bf70f3842150e4a42c36c70e7
ryanybrennan <ryanybrennan@msn.com>
2019-01-18 22:21:35 -0800

    made a modification, see if Travis CI triggers

--- ad440demoApi/ad440demoApi.cs
+++ ad440demoApi/ad440demoApi.cs
@@ -26,7 +26,7 @@ namespace ad440demoApi
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                ? (ActionResult)new OkObjectResult($"Well hello there, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
